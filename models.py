1536da6b0844c3d6ca35192e831ef757febde9b2
Krithik Qfit <krithik-qfit@krithiks-air-2.mynetworksettings.com>
2023-10-03 15:32:14 -0400

    sd

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from datetime import datetime
-from app import db 
+from database import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
