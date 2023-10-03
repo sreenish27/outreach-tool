d63b38ea252b478827ffb0b90be01687d5998407
Krithik Qfit <krithik-qfit@krithiks-air-2.mynetworksettings.com>
2023-10-03 18:01:05 -0400

    numberrange

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional, Regexp, URL
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional, Regexp, URL, NumberRange
 
 
 class RegistrationForm(FlaskForm):
